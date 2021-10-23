77be7171c0bd8bc70cbd77407752de882a5557ce
Troy Wiegel <37552547+troywiegel@users.noreply.github.com>
2021-10-23 14:00:40 -0700

    added some functionality to the getGiphy function

--- Assets/script.js
+++ Assets/script.js
@@ -1,35 +1,38 @@
 let searchEl = $('#movieName')
 
-
-
 $('#searchMovie').on('click', function () {
 
     $.ajax({
         url: 'http://www.omdbapi.com/?apikey=25ec969f&t=' + searchEl.val()
-    }).then(function (data) {
+    }).then(function (response) {
 
         $('.empty').empty()
-        
-        console.log('movie data??', data)
 
-        let title = data.Title
-        let year = data.Year
-        let actors = data.Actors
+        let title = response.Title
+        let year = response.Year
+        let actors = response.Actors
+        let genre = response.Genre
 
         $('#result').append('Title: ' + title)
         $('#result').append('Year it was released: ' + year)
         $('#result').append('Main actors/actresses: ' + actors)
 
     })
+    getGiphy()
 })
 
-function getGiphy(data) {
+function getGiphy(genre) {
 
     $.ajax({
         url: 'https://api.giphy.com/v1/gifs/search?api_key=Yra79OxqQFHvDTCxC6nGYUJfeNW8hjuK&q=scary&limit=25&offset=0&rating=pg-13&lang=en'
-    }).then(function (data) {
-        console.log('giphy data??', data)
+    }).then(function (response) {
+        console.log('giphy data??', response)
+        console.log('scary gif??', response.data[0].bitly_url)
+
+        let gifDiv = $('<img/>')
+        let gif = response.data[0].bitly_url
+        $(gifDiv).attr('src', gif)
+        $('#result').append(gifDiv)
+        
     })
 }
-
-//test
