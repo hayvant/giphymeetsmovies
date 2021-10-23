e2124663cd2c1a7307f437fad32656b577230dce
Troy Wiegel <37552547+troywiegel@users.noreply.github.com>
2021-10-23 10:44:10 -0700

    added parameters to getMovie function

--- Assets/script.js
+++ Assets/script.js
@@ -3,9 +3,12 @@
 function getMovie() {
 
     $.ajax({
-        url: 'http://www.omdbapi.com/?apikey=25ec969f&s=godfather'
+        url: 'http://www.omdbapi.com/?apikey=25ec969f&t=the_godfather'
     }).then(function (data) {
         console.log('movie data??', data)
+        console.log('movie title??', data.Title)
+        console.log('movie year??', data.Year)
+        console.log('movie actors??', data.Actors)
     })
 }
 getMovie()
@@ -15,7 +18,7 @@ function getGiphy(data) {
     $.ajax({
         url: 'https://api.giphy.com/v1/gifs/search?api_key=Yra79OxqQFHvDTCxC6nGYUJfeNW8hjuK&q=scary&limit=25&offset=0&rating=pg-13&lang=en'
     }).then(function (data) {
-        console.log('giphy data??', data)
+        // console.log('giphy data??', data)
     })
 }
 getGiphy()
