acbe6c524b8af44a8eda77de4725c7a86c2534cd
hayvant <89271807+hayvant@users.noreply.github.com>
2021-10-29 14:22:06 -0700

    local storage added

--- Assets/script.js
+++ Assets/script.js
@@ -79,7 +79,9 @@ function getStorage() {
     for (let i = 0; i < movieArray.length; i++) {
 
         let savedMovie = $('<p>')
+        $(savedMovie).attr('class', 'storedMovies bg-gray-400 mx-1 my-0.5 rounded-sm')
         $(savedMovie).text(movieArray[i])
         $('#movieStorage').append(savedMovie)
     }
 }
+
